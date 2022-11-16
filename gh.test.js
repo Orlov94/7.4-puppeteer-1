46307f174f43b3e2f7317ecd93a1c102b77af067
Vladislav <vladislav_orlov_94@mail.ru>
2022-11-16 22:49:58 +0300

    new tests

--- gh.test.js
+++ gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,6 +9,10 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -31,6 +34,31 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Get started with Team");
+    expect(actual).toContain(" Get started with Team");
   }, 6000);
 });
+
+test("The page Marketplace", async () => {
+  await page.goto("https://github.com/marketplace");
+  await page.waitForSelector("h1");
+  const title3 = await page.title();
+  expect(title3).toEqual(
+    "GitHub Marketplace · to improve your workflow · GitHub"
+  );
+}, 50000);
+
+test("The page pricing", async () => {
+  await page.goto("https://github.com/pricing");
+  await page.waitForSelector("h1");
+  const title3 = await page.title();
+  expect(title3).toEqual("Pricing · Plans for every developer · GitHub");
+}, 50000);
+
+test("The home for developer communities", async () => {
+  await page.goto("https://github.com/features/discussions");
+  await page.waitForSelector("h1");
+  const title3 = await page.title();
+  expect(title3).toEqual(
+    "GitHub Discussions · The home for developer communities · GitHub"
+  );
+}, 50000);
